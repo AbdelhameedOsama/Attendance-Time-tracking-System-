22a7e8b5704ffbabe640f26728d76d8452fad85b
Abdelhameed Osama <abdelhameedosama10@gmail.com>
2024-03-31 17:51:48 +0200

    addingLoginAndStudent/InstructorReposAndLayouts

---
+++ Attendance_Time_Tracking/Attendance_Time_Tracking/Repos/StudentRepo.cs
@@ -0,0 +1,17 @@
+﻿using Attendance_Time_Tracking.Data;
+
+namespace Attendance_Time_Tracking.Repos
+{
+	public interface IStudentRepo
+	{
+	}
+	public class StudentRepo : IStudentRepo
+	{
+		readonly AttendanceContext db;
+		public StudentRepo(AttendanceContext _db)
+		{
+			db = _db;
+		}
+
+	}
+}
---
+++ Attendance_Time_Tracking/Attendance_Time_Tracking/Repos/UserRepo.cs
@@ -0,0 +1,32 @@
+﻿using Attendance_Time_Tracking.Data;
+using Attendance_Time_Tracking.Models;
+using System.Security.Claims;
+
+namespace Attendance_Time_Tracking.Repos
+{
+	public interface IUserRepo
+	{
+		User GetUser(string Name, string Password);
+		int GetUserId(ClaimsPrincipal user);
+
+	}
+	public class UserRepo : IUserRepo
+	{
+		readonly AttendanceContext db;
+		public UserRepo(AttendanceContext db)
+		{
+			this.db = db;
+		}
+		public User GetUser(string Email, string Password)
+		{
+			return db.Users.FirstOrDefault(u => u.Email == Email && u.Password == Password);
+		}
+
+		public int GetUserId(ClaimsPrincipal user)
+		{
+			var userIdClaim = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+
+			return Convert.ToInt32(userIdClaim.Value);
+		}
+	}
+}
